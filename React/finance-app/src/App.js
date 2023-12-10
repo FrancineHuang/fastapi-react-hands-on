caa2847f42deb6080035cf8603dad9d9ece5b355
FrancineHuang <huangxin130@gmail.com>
2023-12-10 20:39:48 +0900

    feature: installed react in this project file

---
+++ React/finance-app/src/App.js
@@ -0,0 +1,50 @@
+import React, {useState, useEffect} from 'react'
+import api from './api'
+import logo from './logo.svg';
+import './App.css';
+import { ChakraProvider } from '@chakra-ui/react'
+
+const App = () => {
+  const [transactions, setTransactions] = useState([]);
+  const [formData, setFormData] = useState({
+    amount: '',
+    category: '',
+    description: '',
+    is_income: false,
+    date: ''
+  });
+
+  const fetchTransactions = async () => {
+    const response = await api.get('/transactions/');
+    setTransactions(response.data)
+  };
+
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]:value,
+    });
+  };
+
+  const handleformSubmit = async (event) => {
+    event.preventDefault();
+    await api.post('/transactions/', formData);
+    fetchTransactions();
+    setFormData({
+      amount: '',
+      category: '',
+      description: '',
+      is_income: false,
+      date: ''
+    });
+  }
+
+
+}
+
+export default App;
